eac1f7fba89635b409ab19339c77be342dd6a994
DESKTOP-ELLBCQC\Hunter <herles.pontual@mcom.gov.br>
2022-07-13 23:52:35 -0300

    Exemplo aula 1

---
+++ number.js
@@ -0,0 +1,12 @@
+const numero = 3;
+const num1 = 5;
+const num2 = 7;
+
+console.log (num1 + num2 * numero)  
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.6;
+const numeroPontoFlutuante1 = .5;
+
+console.log(numeroPontoFlutuante + numeroPontoFlutuante1)
---
+++ typeString.js
@@ -0,0 +1,10 @@
+const texto = 'Ola mundo'
+const texto1 = 'oi como "vc está"?'
+const primeiroNome = 'Herles'
+const segundoNome = 'Pontual'
+
+console.log (primeiroNome + ' ' + segundoNome)
+console.log (texto1)
+console.log (texto)
+console.log(texto1.toUpperCase())
+console.log(primeiroNome.length)
